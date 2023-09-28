77b9d82215f9c672d6ce9561777848b797a50eb7
Alvin Mahendra <yusufalvin0799@gmail.com>
2023-09-28 17:23:22 +0700

    add store folder

--- src/contents/Header/Header.tsx
+++ src/contents/Header/Header.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 
-type HeaderProps = {
+declare type HeaderProps = {
   name: string;
 }
 
+type State = {
+  id: number;
+  open: boolean;
+}
+
 const Header = ({name}: HeaderProps): JSX.Element => {
-  return <div>{name}</div>
+  const [state, setState] = React.useState<State>({id: 0, open: true});
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) : void => {
+    setState({
+      ...state,
+      open: !state.open,
+    });
+  }
+
+  return (
+    <div>
+      {state.open && (<div>{name}</div>)}
+      <button onClick={handleClick}>Button</button>
+    </div>
+  );
 }
 
 export default Header;
